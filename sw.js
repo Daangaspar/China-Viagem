31264f49156f7a95659962c923677025fe842283
Daangaspar <daan.gaspar@gmail.com>
2026-04-01 10:24:58 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'china-viagem-v1';
+const FILES = [
+  './china_organizer.html',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).catch(() => caches.match('./china_organizer.html'));
+    })
+  );
+});
